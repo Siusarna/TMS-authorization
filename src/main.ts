b1dcb976d9986c2f8eabe4445d8dda92decba3cb
dima <charodeyrap@gmail.com>
2022-04-22 15:52:45 +0300

    add versioning, validation and prefix

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpStatus, ValidationPipe, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+    }),
+  );
+  app.setGlobalPrefix('api');
   await app.listen(3000);
 }
+
 bootstrap();
