01fde21719275f270171bc02e3f5f53defb66c23
dima <charodeyrap@gmail.com>
2022-04-22 15:52:07 +0300

    add isCommonPassword decorator

---
+++ src/custom-decorators/validators/is-common-password.decorator.ts
@@ -0,0 +1,25 @@
+import {
+  registerDecorator,
+  ValidationOptions,
+} from 'class-validator';
+import isCommonPassword from '../../utils/is-common-password';
+
+export function IsCommonPassword(validationOptions?: ValidationOptions) {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      name: 'isCommonPassword',
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      validator: {
+        validate() {
+          const value = object[propertyName];
+          return isCommonPassword(value);
+        },
+        defaultMessage(): string {
+          return 'This password is common';
+        },
+      },
+    });
+  };
+}
