f602dcfa14168baff26cfd16005f65f558d2d45a
dima <charodeyrap@gmail.com>
2022-04-26 11:40:43 +0300

    WIP, sign-up

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,11 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { Tokens } from './interfaces/tokens.interface';
 import { SignUpDto } from './dtos/sign-up.dto';
+import { AuthService } from "./auth.service";
 
 @Controller('auth')
 export class AuthController {
+  constructor(private authService: AuthService) {}
   @Post()
   signUp(@Body() authInfo: SignUpDto): Tokens {
     return {
